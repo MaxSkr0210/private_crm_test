a727b282fd68af1d066c875e32d792f2b43a97cc
MaxSkr0210 <maxskr0210@mail.ru>
2023-11-24 00:06:03 +0300

    need help
    I have no ideas to solve this issue

---
+++ src/App.vue
@@ -0,0 +1,200 @@
+<template>
+  <div class="main">
+    <table border="1">
+      <tr>
+        <th>Номер</th>
+        <th>Имя</th>
+        <th>Диеты</th>
+        <th>Тариф</th>
+        <th>Адрес</th>
+        <th>Телефон</th>
+        <th>Дата начала - конца диеты</th>
+        <th>Скидка</th>
+        <th>Оплата</th>
+        <th>Курьеру</th>
+        <th>Примечание</th>
+        <th @click="sort()">Статус</th>
+      </tr>
+      <table_row
+        v-for="diet in diets"
+        :key="diet.o_id"
+        :diet="diet"></table_row>
+    </table>
+  </div>
+</template>
+
+<script>
+import data from "../data.json";
+import table_row from "@/components/table_row.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      diets: [], // немного изменные начальные данные
+      fullDiets: [], // разделенные данные по времени, те у одного объекта по одному тарифу, времени, плану
+      sorted: false, // флаг для сортировки
+    };
+  },
+  components: {
+    table_row,
+  },
+  methods: {
+    // фильтр массивов (временный)
+    filterArray(arr, key) {
+      return arr.filter((item) => item.time.indexOf(key) >= 0);
+    },
+    // функция для метода sort, сортировка по убыванию
+    compareFnDesc(a, b) {
+      const _a = parseInt(a.status.match(/\d+/));
+      const _b = parseInt(b.status.match(/\d+/));
+      if (_a < _b) return 1;
+      if (_a > _b) return -1;
+      return 0;
+    },
+    // функция для метода sort, сортировка по возрастанию
+    compareFnDAbs(a, b) {
+      const _a = parseInt(a.status.match(/\d+/));
+      const _b = parseInt(b.status.match(/\d+/));
+      if (_a > _b) return 1;
+      if (_a < _b) return -1;
+      return 0;
+    },
+    //сортировка диет
+    sort() {
+      if (this.sorted) {
+        this.diets = this.diets.reverse();
+        console.log(this.diets);
+      } else {
+        this.diets = this.fullDiets;
+
+        //Идет разделение массива на несколько категорий, каждая из которых в процессе сортируется и потом, полученные массивы объединяются.
+
+        const begins = this.filterArray(this.diets, "Начинается через").sort(
+          this.compareFnDesc
+        );
+        const endsToday = this.filterArray(this.diets, "Заканчивается сегодня");
+        const endsTomorrow = this.filterArray(
+          this.diets,
+          "Заканчивается завтра"
+        );
+        const endsSoon = this.filterArray(
+          this.diets,
+          "Заканчивается через"
+        ).sort(this.compareFnDAbs);
+        const alreadyEnds = this.filterArray(this.diets, "Закончилось").sort(
+          this.compareFnDAbs
+        );
+        this.diets = [
+          ...begins,
+          ...endsToday,
+          ...endsTomorrow,
+          ...endsSoon,
+          ...alreadyEnds,
+        ];
+
+        this.sorted = true;
+      }
+    },
+
+    // Формирование столбца Статус
+    entry_time(start_date, end_date) {
+      const today = new Date().getTime();
+      const sd = new Date(start_date).getTime();
+      const ed = new Date(end_date).getTime();
+      let res = "";
+
+      if (today < sd) {
+        const timeDiff = Math.abs(sd - today);
+        const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        res = `Начинается через ${diffDays} дней`;
+      } else if (today > sd && ed - today === 0) {
+        res = "Заканчивается сегодня";
+      } else if (today > sd && ed - today === 1) {
+        res = "Заканчивается завтра";
+      } else if (today > sd && ed > today) {
+        const timeDiff = Math.abs(ed - today);
+        const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        res = `Заканчивается через ${diffDays} дней`;
+      } else if (ed < today) {
+        const timeDiff = Math.abs(today - ed);
+        const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        res = `Закончилось ${diffDays} дней назад`;
+      }
+
+      return res;
+    },
+  },
+  created() {
+    //изменение изначальных данных и запись в переменную diets
+    for (let i = 0; i < data.length; i++) {
+      //добавление столбца pay_info, который объединяет order_sum, order_payed и pay_status
+      data[i].pay_info = `${data[i].order_sum}, ${data[i].pay_status}, долг=${
+        data[i].order_sum - Number(data[i].order_payed)
+      }`;
+
+      const courier_comment = data[i].courier_comment;
+      const inner_comment = data[i].inner_comment;
+
+      delete data[i].courier_comment;
+      delete data[i].inner_comment;
+      delete data[i].order_sum;
+      delete data[i].order_payed;
+      delete data[i].pay_status;
+
+      data[i].courier_comment = courier_comment;
+      data[i].inner_comment = inner_comment;
+    }
+
+    this.diets = data;
+
+    // добавление столбца status для понимания, когда начнется или закончится та или иная диета
+    for (let i = 0; i < this.diets.length; i++) {
+      this.diets[i].status = [];
+      for (let j = 0; j < this.diets[i].dates.length; j++) {
+        const entry_time = this.entry_time(
+          this.diets[i].dates[j].start_date,
+          this.diets[i].dates[j].end_date
+        );
+
+        this.diets[i].status.push(entry_time);
+
+        const forFullDiets = structuredClone(this.diets[i]);
+
+        forFullDiets.diets = [this.diets[i].diets[j]];
+        forFullDiets.tariff = [this.diets[i].tariff[j]];
+        forFullDiets.dates = [this.diets[i].dates[j]];
+        forFullDiets.status = this.diets[i].status[j];
+
+        this.fullDiets.push(forFullDiets);
+      }
+    }
+  },
+  mounted() {
+    // исправление размеров ячеек таблицы (временно)
+    const trs = document.querySelectorAll(".item");
+
+    for (let i = 0; i < trs.length; i++) {
+      const childrens = trs[i].children;
+      childrens[0].width = "60px;";
+      childrens[1].width = "150px;";
+      childrens[2].width = "60px;";
+      childrens[3].width = "60px";
+      childrens[4].width = "180px;";
+      childrens[5].width = "120px;";
+      childrens[6].width = "120px;";
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+table {
+  width: 100%;
+}
+
+td,
+th {
+  word-wrap: break-word;
+}
+</style>
---
+++ src/components/table_row.vue
@@ -0,0 +1,30 @@
+<template>
+  <tr class="item">
+    <td v-for="(value, key) in diet" :key="key" align="center">
+      <span v-if="typeof value === 'number' || typeof value === 'string'">
+        {{ value }}
+      </span>
+      <tr v-else v-for="(item, index) in value" :key="item" class="subitem">
+        <span v-if="typeof item === 'string' || typeof item === 'number'">
+          {{ item }}
+        </span>
+        <span v-else>{{ item.start_date }} - {{ item.end_date }}</span>
+        <hr v-if="value.length - index !== 1" />
+      </tr>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    diet: {},
+  },
+};
+</script>
+
+<style lang="scss">
+.subitem {
+  width: 100%;
+}
+</style>
